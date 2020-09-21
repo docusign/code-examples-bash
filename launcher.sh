c9aa227c0a1e15896ffb2f50c916bae7f290f2ba
Aaron Wilde <Aaron.Wilde@docusign.com>
2020-09-21 13:23:33 -0700

    updated launcher.sh and file folder locations

--- launcher.sh
+++ launcher.sh
@@ -117,97 +117,97 @@ case "$CHOICE" in
 Home) 
 login
 ;;
-Embedded_Signing) bash examples/eg001EmbeddedSigning.sh 
+Embedded_Signing) bash eg001EmbeddedSigning.sh 
 continu
 ;;
-Signing_Via_Email) bash examples/eg002SigningViaEmail.sh 
+Signing_Via_Email) bash examples/eSignature/eg002SigningViaEmail.sh 
 continu
 ;;
-List_Envelopes) bash examples/eg003ListEnvelopes.sh 
+List_Envelopes) bash examples/eSignature/eg003ListEnvelopes.sh 
 continu
 ;;
-Envelope_Info) bash examples/eg004EnvelopeInfo.sh 
+Envelope_Info) bash examples/eSignature/eg004EnvelopeInfo.sh 
 continu
 ;;
-Envelope_Recipients) bash examples/eg005EnvelopeRecipients.sh 
+Envelope_Recipients) bash examples/eSignature/eg005EnvelopeRecipients.sh 
 continu
 ;;
-Envelope_Docs) bash examples/eg006EnvelopeDocs.sh 
+Envelope_Docs) bash examples/eSignature/eg006EnvelopeDocs.sh 
 continu
 ;;
-Envelope_Get_Doc) bash examples/eg007EnvelopeGetDoc.sh 
+Envelope_Get_Doc) bash examples/eSignature/eg007EnvelopeGetDoc.sh 
 continu
 ;;
-Create_Template) bash examples/eg008CreateTemplate.sh 
+Create_Template) bash examples/eSignature/eg008CreateTemplate.sh 
 continu
 ;;
-Use_Template) bash examples/eg009UseTemplate.sh 
+Use_Template) bash examples/eSignature/eg009UseTemplate.sh 
 continu
 ;;
-Send_Binary_Docs) bash examples/eg010SendBinaryDocs.sh 
+Send_Binary_Docs) bash examples/eSignature/eg010SendBinaryDocs.sh 
 continu
 ;;
-Embedded_Sending) bash examples/eg011EmbeddedSending.sh 
+Embedded_Sending) bash examples/eSignature/eg011EmbeddedSending.sh 
 continu
 ;;
-Embedded_Console) bash examples/eg012EmbeddedConsole.sh 
+Embedded_Console) bash examples/eSignature/eg012EmbeddedConsole.sh 
 continu
 ;;
-Add_Doc_To_Template) bash examples/eg013AddDocToTemplate.sh 
+Add_Doc_To_Template) bash examples/eSignature/eg013AddDocToTemplate.sh 
 continu
 ;;
-Collect_Payment) bash examples/eg014CollectPayment.sh 
+Collect_Payment) bash examples/eSignature/eg014CollectPayment.sh 
 continu
 ;;
-Envelope_Tab_Data) bash examples/eg015EnvelopeTabData.sh 
+Envelope_Tab_Data) bash examples/eSignature/eg015EnvelopeTabData.sh 
 continu
 ;;
-Set_Tab_Values) bash examples/eg016SetTabValues.sh 
+Set_Tab_Values) bash examples/eSignature/eg016SetTabValues.sh 
 continu
 ;;
-Set_Template_Tab_Values) bash examples/eg017SetTemplateTabValues.sh 
+Set_Template_Tab_Values) bash examples/eSignature/eg017SetTemplateTabValues.sh 
 continu
 ;;
-Envelope_Custom_Field_Data) bash examples/eg018EnvelopeCustomFieldData.sh 
+Envelope_Custom_Field_Data) bash examples/eSignature/eg018EnvelopeCustomFieldData.sh 
 continu
 ;;
-Signing_Via_Email_With_Access_Code) bash examples/eg019SigningViaEmailWithAccessCode.sh 
+Signing_Via_Email_With_Access_Code) bash examples/eSignature/eg019SigningViaEmailWithAccessCode.sh 
 continu
 ;;
-Signing_Via_Email_With_Sms_Authentication) bash examples/eg020SigningViaEmailWithSmsAuthentication.sh 
+Signing_Via_Email_With_Sms_Authentication) bash examples/eSignature/eg020SigningViaEmailWithSmsAuthentication.sh 
 continu
 ;;
-Signing_Via_Email_With_Phone_Authentication) bash examples/eg021SigningViaEmailWithPhoneAuthentication.sh 
+Signing_Via_Email_With_Phone_Authentication) bash examples/eSignature/eg021SigningViaEmailWithPhoneAuthentication.sh 
 continu
 ;;
-Signing_Via_Email_With_Knoweldge_Based_Authentication) bash examples/eg022SigningViaEmailWithKnoweldgeBasedAuthentication.sh 
+Signing_Via_Email_With_Knoweldge_Based_Authentication) bash examples/eSignature/eg022SigningViaEmailWithKnoweldgeBasedAuthentication.sh 
 continu
 ;;
-Signing_Via_Email_With_IDV_Authentication) bash examples/eg023SigningViaEmailWithIDVAuthentication.sh 
+Signing_Via_Email_With_IDV_Authentication) bash examples/eSignature/eg023SigningViaEmailWithIDVAuthentication.sh 
 continu
 ;;
-Creating_Permission_Profiles) bash examples/eg024CreatingPermissionProfiles.sh 
+Creating_Permission_Profiles) bash examples/eSignature/eg024CreatingPermissionProfiles.sh 
 continu
 ;;
-Setting_Permission_Profiles) bash examples/eg025SettingPermissionProfiles.sh 
+Setting_Permission_Profiles) bash examples/eSignature/eg025SettingPermissionProfiles.sh 
 continu
 ;;
-Updating_Individual_Permission) bash examples/eg026UpdatingIndividualPermission.sh 
+Updating_Individual_Permission) bash examples/eSignature/eg026UpdatingIndividualPermission.sh 
 continu
 ;;
-Deleting_Permissions) bash examples/eg027DeletingPermissions.sh 
+Deleting_Permissions) bash examples/eSignature/eg027DeletingPermissions.sh 
 continu
 ;;
-Creating_A_Brand) bash examples/eg028CreatingABrand.sh 
+Creating_A_Brand) bash examples/eSignature/eg028CreatingABrand.sh 
 continu
 ;;
-Applying_Brand_Envelope) bash examples/eg029ApplyingBrandEnvelope.sh 
+Applying_Brand_Envelope) bash examples/eSignature/eg029ApplyingBrandEnvelope.sh 
 continu
 ;;
-Applying_Brand_Template) bash examples/eg030ApplyingBrandTemplate.sh 
+Applying_Brand_Template) bash examples/eSignature/eg030ApplyingBrandTemplate.sh 
 continu
 ;;
-Bulk_Sending) bash examples/eg031BulkSending.sh 
+Bulk_Sending) bash examples/eSignature/eg031BulkSending.sh 
 continu
 ;;
 *) echo "Default action..."
