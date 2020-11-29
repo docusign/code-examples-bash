ad9476ffaea4101a039b1e25fc10d236b759b1e9
Serhii Ovsianikov <serhii.ovsianikov@sigma.software>
2020-11-29 23:47:15 +0200

    Updated eg004

--- examples/Click/eg004EmbedClickwrap.sh
+++ examples/Click/eg004EmbedClickwrap.sh
@@ -18,3 +18,24 @@ access_token=$(cat config/ds_access_token.txt)
 #    the drop down on the upper right corner of the screen by your picture or
 #    the default picture.
 account_id=$(cat config/API_ACCOUNT_ID)
+
+# Get a ClickWrap ID
+if [ -f "config/CLICKWRAP_ID" ]; then
+    clickwrap_id=$(cat config/CLICKWRAP_ID)
+else
+    echo ""
+    echo "ClickWrap ID is neded. Please run step 1 and 2 - Create and Activate ClickWrap..."
+    exit 0
+fi
+
+echo "Write the code you need to embed your clickwrap in JavaScript. It should include the following HTML tags:"
+echo "
+<div id=\"ds-clickwrap\"></div>
+<script src=\"https://demo.docusign.net/clickapi/sdk/latest/docusign-click.js\"></script>
+<script>docuSignClick.Clickwrap.render({
+      environment: 'https://demo.docusign.net',
+      accountId: '${account_id}',
+      clickwrapId: '${clickwrap_id}',
+      clientUserId: 'UNIQUE_USER_ID'
+    }, '#ds-clickwrap');</script>
+"
