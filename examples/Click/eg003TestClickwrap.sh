7b67cf2c532c48c3c52a30ebb67dee60b91dd3d8
Serhii Ovsianikov <serhii.ovsianikov@sigma.software>
2020-12-02 00:46:52 +0200

    Added check of ClickWrap

--- examples/Click/eg003TestClickwrap.sh
+++ examples/Click/eg003TestClickwrap.sh
@@ -28,9 +28,30 @@ else
     exit 0
 fi
 
+# Check that Clickwrap is activated
+echo ""
+echo "Check that Clickwrap is activated..."
+
+response=$(mktemp /tmp/response-cw.XXXXXX)
+declare -a Headers=('--header' "Authorization: Bearer ${access_token}"
+    '--header' "Accept: application/json")
+
+Status=$(curl --request GET https://demo.docusign.net/clickapi/v1/accounts/${account_id}/clickwraps/${clickwrap_id}/ \
+    "${Headers[@]}" \
+    --output ${response})
+
+clickwrap_status=$(cat $response | grep status | sed 's/ //g' | sed 's/.*\"status\":\"//' | sed 's/\",.*//')
+
+if [ $clickwrap_status != "active" ]; then
+    echo ""
+    echo "ClickWrap ID is not activated. Please run step 2 - Activate ClickWrap..."
+    exit 0
+fi
+
 environment="demo"
 test_url="https://developers.docusign.com/click-api/test-clickwrap?a=${account_id}&cw=${clickwrap_id}&eh=${environment}"
 
+echo ""
 printf "The clickwrap tester URL is ${test_url}\n"
 printf "Opening the clickwrap tester URL directly in your browser...\n"
 if which xdg-open &>/dev/null; then
@@ -40,3 +61,6 @@ elif which open &>/dev/null; then
 elif which start &>/dev/null; then
     start "$test_url"
 fi
+
+# Remove the temporary file
+rm "$response"
