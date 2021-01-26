e1256bf1fde4eb7e02f3a15631cc3ef8917b480d
robert-knight <robert.knight@docusign.com>
2021-01-26 13:32:55 -0800

    Add eg035

---
+++ examples/eSignature/eg035SigningViaSMS.sh
@@ -0,0 +1,176 @@
+# Send an envelope with three documents
+#
+# Check that we're in a bash shell
+if [[ $SHELL != *"bash"* ]]; then
+  echo "PROBLEM: Run these scripts from within the bash shell."
+fi
+
+# Check for a valid cc email and prompt the user if 
+#CC_EMAIL and CC_NAME haven't been set in the config file.
+source ./examples/eSignature/lib/utils.sh
+CheckForValidCCEmail
+
+# Prompt for a valid 
+
+# Step 1: Obtain your OAuth token
+# Note: Substitute these values with your own
+ACCESS_TOKEN=$(cat config/ds_access_token.txt)
+
+# Set up variables for full code example
+# Note: Substitute these values with your own
+account_id=$(cat config/API_ACCOUNT_ID)
+
+base_path="https://demo.docusign.net/restapi"
+
+# ***DS.snippet.0.start
+#  document 1 (html) has tag **signature_1**
+#  document 2 (docx) has tag /sn1/
+#  document 3 (pdf) has tag /sn1/
+#
+#  The envelope has two recipients.
+#  recipient 1 - signer
+#  recipient 2 - cc
+#  The envelope will be sent first to the signer.
+#  After it is signed, a copy is sent to the cc person.
+
+# temp files:
+request_data=$(mktemp /tmp/request-eg-002.XXXXXX)
+response=$(mktemp /tmp/response-eg-002.XXXXXX)
+doc1_base64=$(mktemp /tmp/eg-002-doc1.XXXXXX)
+doc2_base64=$(mktemp /tmp/eg-002-doc2.XXXXXX)
+doc3_base64=$(mktemp /tmp/eg-002-doc3.XXXXXX)
+
+# Fetch docs and encode
+cat demo_documents/doc_1.html | base64 > $doc1_base64
+cat demo_documents/World_Wide_Corp_Battle_Plan_Trafalgar.docx | base64 > $doc2_base64
+cat demo_documents/World_Wide_Corp_lorem.pdf | base64 > $doc3_base64
+
+echo ""
+echo "Sending the envelope request to DocuSign..."
+echo "The envelope has three documents. Processing time will be about 15 seconds."
+echo "Results:"
+echo ""
+
+# Concatenate the different parts of the request
+printf \
+'{
+    "emailSubject": "Please sign this document set",
+    "documents": [
+        {
+            "documentBase64": "' > $request_data
+            cat $doc1_base64 >> $request_data
+            printf '",
+            "name": "Order acknowledgement",
+            "fileExtension": "html",
+            "documentId": "1"
+        },
+        {
+            "documentBase64": "' >> $request_data
+            cat $doc2_base64 >> $request_data
+            printf '",
+            "name": "Battle Plan",
+            "fileExtension": "docx",
+            "documentId": "2"
+        },
+        {
+            "documentBase64": "' >> $request_data
+            cat $doc3_base64 >> $request_data
+            printf '",
+            "name": "Lorem Ipsum",
+            "fileExtension": "pdf",
+            "documentId": "3"
+        }
+    ],
+    "recipients": 
+    {
+        "carbonCopies": 
+        [
+            {
+             "additionalNotifications":
+             [
+                {
+                   "phoneNumber":
+                   {
+                      "countryCode": "'"${CC_PHONE_COUNTRY}"'",
+                      "number":"'"${CC_PHONE_NUMBER}"'"
+                   },
+                   "secondaryDeliveryMethod":"SMS"
+                }
+             ],                
+            "email": "'"${CC_EMAIL}"'",
+            "name": "'"${CC_NAME}"'",
+            "recipientId": "2",
+            "routingOrder": "2"
+            }
+        ],
+        "signers": 
+        [
+            {
+             "additionalNotifications":
+             [
+                {
+                   "phoneNumber":
+                   {
+                      "countryCode": "'"${SIGNER_PHONE_COUNTRY}"'",
+                      "number":"'"${SIGNER_PHONE_NUMBER}"'"
+                   },
+                   "secondaryDeliveryMethod":"SMS"
+                }
+             ],                
+                "email": "'"${SIGNER_EMAIL}"'",
+                "name": "'"${SIGNER_NAME}"'",
+                "recipientId": "1",
+                "routingOrder": "1",
+                "tabs": 
+                {
+                    "signHereTabs": 
+                    [
+                        {
+                            "anchorString": "**signature_1**",
+                            "anchorUnits": "pixels",
+                            "anchorXOffset": "20",
+                            "anchorYOffset": "10"
+                        },
+                        {
+                            "anchorString": "/sn1/",
+                            "anchorUnits": "pixels",
+                            "anchorXOffset": "20",
+                            "anchorYOffset": "10"
+                        }
+                    ]
+                }
+            }
+        ]
+    },
+    "status": "sent"
+}' >> $request_data
+
+curl --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+     --header "Content-Type: application/json" \
+     --data-binary @${request_data} \
+     --request POST ${base_path}/v2.1/accounts/${account_id}/envelopes \
+     --output $response
+
+echo ""
+echo "Response:"
+cat $response
+echo ""
+
+# pull out the envelopeId
+envelope_id=`cat $response | grep envelopeId | sed 's/.*\"envelopeId\":\"//' | sed 's/\",.*//'`
+# ***DS.snippet.0.end
+# Save the envelope id for use by other scripts
+echo "EnvelopeId: ${envelope_id}"
+echo ${envelope_id} > config/ENVELOPE_ID
+
+# cleanup
+rm "$request_data"
+rm "$response"
+rm "$doc1_base64"
+rm "$doc2_base64"
+rm "$doc3_base64"
+
+echo ""
+echo ""
+echo "Done."
+echo ""
--- examples/eSignature/lib/utils.sh
+++ examples/eSignature/lib/utils.sh
@@ -32,5 +32,45 @@ while [[ $SIGNER_NOT_CHECKED_EMAIL != *"@"* ]]; do
 done
 }
 
+function CheckForValidCCPhoneNum()
+{
+  if [[ $CC_PHONE_COUNTRY != (0|[1-9][0-9]?|998)$ || $CC_PHONE_COUNTRY == "" \
+        $CC_PHONE_NUMBER != ^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$ || $CC_PHONE_NUMBER == "" ]]; then
+      echo ""
+      read -p "Enter CC phone country code (U.S. is 1): " CC_PHONE_COUNTRY
+      read -p "Enter CC phone number: " CC_PHONE_NUMBER
+      CC_PHONE_NUMBER=`cat $CC_PHONE_NUMBER | sed 's/[^0-9]*//g'`
+      while [[ $CC_PHONE_COUNTRY != (0|[1-9][0-9]?|998)$ || $CC_PHONE_COUNTRY == "" \
+                $CC_PHONE_NUMBER != ^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$ || $CC_PHONE_NUMBER == ""  ]] ; do
+      echo ""
+      read -p "Enter CC phone country code (U.S. is 1): " CC_PHONE_COUNTRY
+      read -p "Enter CC phone number: " CC_PHONE_NUMBER
+      done
+      echo ""
+      echo "CC_PHONE_COUNTRY is " $CC_PHONE_COUNTRY
+      echo "CC_NAME is " $CC_NAME
+fi
+}
+
+function CheckForValidRecipPhoneNum()
+{
+while [[ $SIGNER_NOT_CHECKED_EMAIL != *"@"* ]]; do
+    echo ""
+    echo "Current signer not checked email is " $SIGNER_NOT_CHECKED_EMAIL
+    read -p "Enter an email address to route to when the checkbox is not checked: " SIGNER_NOT_CHECKED_EMAIL
+    if [[ $SIGNER_NOT_CHECKED_NAME == *"{"* || SIGNER_NOT_CHECKED_NAME == "" ]] ; then
+        echo ""
+        echo "Current signer not checked name is " $SIGNER_NOT_CHECKED_NAME
+        read -p "Enter a name for the recipient when the checkbox is not checked: " SIGNER_NOT_CHECKED_NAME
+    fi
+    echo ""
+    echo "SIGNER_NOT_CHECKED_EMAIL is " $SIGNER_NOT_CHECKED_EMAIL
+    echo "SIGNER_NOT_CHECKED_NAME is " $SIGNER_NOT_CHECKED_NAME
+done
+}
+
+
 export -f CheckForValidCCEmail
 export -f CheckForValidNotCheckedEmail
+export -f CheckForValidCCPhoneNum
+export -f CheckForValidRecipPhoneNum
