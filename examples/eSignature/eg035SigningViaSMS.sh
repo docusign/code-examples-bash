bb3577b1f499d8a7df02115c4049136b61277ade
robert-knight <robert.knight@docusign.com>
2021-01-26 23:26:33 -0800

    Added SMS example and updated endpoint for bulk send verification.

--- examples/eSignature/eg035SigningViaSMS.sh
+++ examples/eSignature/eg035SigningViaSMS.sh
@@ -10,7 +10,11 @@ fi
 source ./examples/eSignature/lib/utils.sh
 CheckForValidCCEmail
 
-# Prompt for a valid 
+# Get a country code and phone number for the CC recipient.
+GetCCPhoneNum
+
+# Get a country code and phone number for the signer
+GetSignerPhoneNum
 
 # Step 1: Obtain your OAuth token
 # Note: Substitute these values with your own
