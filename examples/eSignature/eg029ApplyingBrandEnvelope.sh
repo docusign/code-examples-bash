0f07142c94eb150207b1db8fd0c88b491b9ac813
Paige Rossi <paige.rossi@docusign.com>
2023-03-24 11:28:26 -0700

    DEVDOCS-10117 adding codedepot comments to eg029

--- examples/eSignature/eg029ApplyingBrandEnvelope.sh
+++ examples/eSignature/eg029ApplyingBrandEnvelope.sh
@@ -24,12 +24,16 @@ brand_id=$brand_id
 base_path="https://demo.docusign.net/restapi"
 
 #Step 2: Construct your API headers
+#ds-snippet-start:eSign29Step2
 declare -a Headers=('--header' "Authorization: Bearer ${ACCESS_TOKEN}" \
 					'--header' "Accept: application/json" \
 					'--header' "Content-Type: application/json")
 
+#ds-snippet-end:eSign29Step2
+
 # Step 3: Construct the request body
 # Create a temporary file to store the request body
+#ds-snippet-start:eSign29Step3
 request_data=$(mktemp /tmp/request-brand-001.XXXXXX)
 printf \
 '{
@@ -68,15 +72,18 @@ printf \
 	"brandId": "'$brand_id'",
 	"status": "Sent"
 }' >> $request_data
+#ds-snippet-end:eSign29Step3
 
 # Step 4: a) Call the eSignature API
 #         b) Display the JSON response
 # Create a temporary file to store the response
 response=$(mktemp /tmp/response-brand.XXXXXX)
+#ds-snippet-start:eSign29Step4
 Status=$(curl -w '%{http_code}' -i --request POST ${base_path}/v2.1/accounts/${account_id}/envelopes \
      "${Headers[@]}" \
      --data-binary @${request_data} \
      --output ${response})
+#ds-snippet-end:eSign29Step4
 # If the Status code returned is greater than 399, display an error message along with the API response
 if [[ "$Status" -gt "399" ]] ; then
     echo ""
