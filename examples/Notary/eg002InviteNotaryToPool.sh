cdce54c8de737ad2437ce63b6c6bb17800291972
meihDS <70775251+meihDS@users.noreply.github.com>
2023-07-17 12:06:37 -0700

    Update eg002InviteNotaryToPool.sh
    deleted old step comments

--- examples/Notary/eg002InviteNotaryToPool.sh
+++ examples/Notary/eg002InviteNotaryToPool.sh
@@ -5,7 +5,7 @@ if [[ $SHELL != *"bash"* ]]; then
   echo "PROBLEM: Run these scripts from within the bash shell."
 fi
 
-# Step 1: Obtain your OAuth token
+# Obtain your OAuth token
 # Note: Substitute these values with your own
 ACCESS_TOKEN=$(cat config/ds_access_token.txt)
 
@@ -19,15 +19,12 @@ base_path="https://notary-d.docusign.net/restapi"
 response=$(mktemp /tmp/response-notary.XXXXXX)
 
 # Construct your API headers
-# Step 2 start
 #ds-snippet-start:Notary2Step2
 declare -a Headers=('--header' "Authorization: Bearer ${ACCESS_TOKEN}" \
     '--header' "Accept: application/json" \
     '--header' "Content-Type: application/json")
 #ds-snippet-end
-# Step 2 end
 
-# Step 3 start
 #ds-snippet-start:Notary2Step3
 Status=$(curl -w '%{http_code}' -i --request GET ${base_path}/v1.0/organizations/${ORGANIZATION_ID}/pools \
      "${Headers[@]}" \
@@ -35,22 +32,15 @@ Status=$(curl -w '%{http_code}' -i --request GET ${base_path}/v1.0/organizations
 
 POOL_ID=$(cat "$response" | sed -n 's/.*"poolId":"\([^"]*\)".*/\1/p')
 #ds-snippet-end
-# Step 3 end
-
 
 # Create a temporary file to store the request data
 request_data=$(mktemp /tmp/request-notary-002.XXXXXX)
 
-# Step 4 start
-#ds-snippet-start:Notary2Step4
 echo ""
 read -p "Please enter a username for the new user: " NOTARY_NAME
 read -p "Please enter an email for the new user: " NOTARY_EMAIL
-#ds-snippet-end
-# Step 4 end
 
-#Step 5 start
-#ds-snippet-start:Admin2Step5
+#ds-snippet-start:Notary2Step4
 printf \
 '
 {
@@ -58,7 +48,6 @@ printf \
   "name" : "'"${NOTARY_NAME}"'",
 }' >$request_data
 #ds-snippet-end
-# Step 5 end
 
 echo ""
 echo "Inviting ${NOTARY_NAME} to your organization's notary pool"
@@ -66,11 +55,12 @@ echo ""
 echo "Pool id is ${POOL_ID}"
 echo ""
 
+#ds-snippet-start:Notary2Step5
 Status=$(curl -w '%{http_code}' -i --request POST ${base_path}/v1.0/organizations/${ORGANIZATION_ID}/pools/${POOL_ID}/invites \
      "${Headers[@]}" \
      --data-binary @${request_data} \
      --output ${response})
-
+#ds-snippet-end
 
 if [[ "$Status" -gt "201" ]]; then
     echo ""
