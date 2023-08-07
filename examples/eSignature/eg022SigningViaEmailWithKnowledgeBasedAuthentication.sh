72a8759b461382199126e291ab392b7d2212516f
Inbar Gazit <inbar.gazit@docusign.com>
2023-08-07 16:15:36 -0700

    DEVDOCS-10147 - adding codeDepot markers

--- examples/eSignature/eg022SigningViaEmailWithKnowledgeBasedAuthentication.sh
+++ examples/eSignature/eg022SigningViaEmailWithKnowledgeBasedAuthentication.sh
@@ -16,10 +16,12 @@ account_id=$(cat config/API_ACCOUNT_ID)
 base_path="https://demo.docusign.net/restapi"
 
 # Step 2: Construct your API headers
+#ds-snippet-start:eSign22Step2
 declare -a Headers=('--header' "Authorization: Bearer ${ACCESS_TOKEN}" \
 					'--header' "Accept: application/json" \
 					'--header' "Content-Type: application/json")
 
+#ds-snippet-end:eSign22Step2
 # Step 3: Construct your envelope JSON body
 # Create a temporary file to store the JSON body
 
@@ -27,6 +29,7 @@ doc_base64=$(mktemp /tmp/eg-019-doc1.XXXXXX)
 cat demo_documents/World_Wide_Corp_Battle_Plan_Trafalgar.docx | base64 > $doc_base64
 
 request_data=$(mktemp /tmp/request-cw.XXXXXX)
+#ds-snippet-start:eSign22Step3
 printf \
 '{
 	"documents": [{
@@ -63,6 +66,7 @@ printf \
 	},
 	"status": "Sent"
 }' >> $request_data				
+#ds-snippet-end:eSign22Step3
 					
 # Step 4: a) Make a POST call to the createEnvelopes endpoint to create a new envelope.
 #         b) Display the JSON structure of the created envelope
@@ -72,10 +76,12 @@ echo ""
 cat $request_data
 # Create a temporary file to store the response
 response=$(mktemp /tmp/response-cw.XXXXXX)
+#ds-snippet-start:eSign22Step4
 curl --request POST "https://demo.docusign.net/restapi/v2.1/accounts/${account_id}/envelopes" \
      "${Headers[@]}" \
      --data-binary @${request_data} \
      --output ${response}
+#ds-snippet-end:eSign22Step4
 
 echo ""
 echo "Response:"
