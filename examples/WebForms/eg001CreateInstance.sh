d00c99df36a754c042c8ce912f528d0812894c6a
Paige Rossi <70241979+paigesrossi@users.noreply.github.com>
2024-06-06 15:22:42 -0700

    Update upload web forms text (#91)

--- examples/WebForms/eg001CreateInstance.sh
+++ examples/WebForms/eg001CreateInstance.sh
@@ -25,7 +25,7 @@ echo $result > demo_documents/web-form-config.json
 
 
 echo "" 
-echo "Go to your DocuSign account to create the Web Form. Go to 'Forms' in your developer account, select 'New,' and choose 'Upload web form.' Upload the JSON config file 'web-form-config.json' found under the demo_documents folder of this project. You will need to activate the web form before proceeding. Press 1 to continue after doing so."
+echo "Go to your Docusign account to create the Web Form. Go to 'Templates' in your developer account, select 'Start,' select 'Web Forms,' and choose 'Upload Web Form.' Upload the JSON config file 'web-form-config.json' found under the demo_documents folder of this project. You will need to activate the web form before proceeding. Press Continue after doing so."
 read choice
 #ds-snippet-start:WebForms1Step2
 declare -a Headers=('--header' "Authorization: Bearer ${ACCESS_TOKEN}" \
