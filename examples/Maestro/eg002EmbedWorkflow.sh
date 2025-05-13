b0f5e8c1314a3d076c2566fd46f8fe3e3caf10f4
nianiB9 <nianibyrd@yahoo.com>
2025-05-13 15:55:13 -0500

    fix code snippets

--- examples/Maestro/eg002EmbedWorkflow.sh
+++ examples/Maestro/eg002EmbedWorkflow.sh
@@ -13,16 +13,16 @@ if [ -z "$workflow_created" ]; then
     exit 0
 fi
 
-#ds-snippet-start:Maestro1Step2
+#ds-snippet-start:Maestro2Step2
 #Check that the instance URL exists
 instance_url=$(cat config/INSTANCE_URL)
 if [ -z "$instance_url" ]; then
     echo "No instance URL found. Please run the trigger workflow script first."
     exit 1
 fi
-#ds-snippet-end:Maestro1Step2
+#ds-snippet-end:Maestro2Step2
 
-#ds-snippet-start:Maestro1Step3
+#ds-snippet-start:Maestro2Step3
 # Launch local server and open in browser
 decoded_instance_url=$(echo "$instance_url" | sed 's/\\u0026/\&/g')
 
@@ -36,4 +36,4 @@ elif which start &> /dev/null   ; then
   start $host_url
 fi
 php ./examples/Maestro/lib/startServerForEmbeddedWorkflow.php "$decoded_instance_url"
-#ds-snippet-end:Maestro1Step3
+#ds-snippet-end:Maestro2Step3
