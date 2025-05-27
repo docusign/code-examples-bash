9c99ff65e58545c575419477949755a073019361
nianiB9 <nianibyrd@yahoo.com>
2025-05-27 15:04:19 -0500

    change code snippets and variable name

--- examples/Maestro/eg001TriggerWorkflow.sh
+++ examples/Maestro/eg001TriggerWorkflow.sh
@@ -174,7 +174,6 @@ echo "Opening a browser with the embedded workflow..."
 
 sleep 5
 
-#ds-snippet-start:Maestro1Step6
 # [Optional] Launch local server and embed workflow instance using the instance URL
 decoded_instance_url=$(echo "$instance_url" | sed 's/\\u0026/\&/g')
 
@@ -188,7 +187,6 @@ elif which start &> /dev/null   ; then
   start $host_url
 fi
 php ./examples/Maestro/lib/startServerForEmbeddedWorkflow.php "$decoded_instance_url"
-#ds-snippet-end:Maestro1Step6
 
 # Remove the temporary files
 rm "$request_data"
