815afa2e88651e9224d291c15490c59d49fa98e0
Raileen Del Rosario <raileen.del@docusign.com>
2025-06-17 15:12:52 -0700

    api ref tags, matching variables to how-to, fixing typos

--- examples/Maestro/eg004CancelWorkflow.sh
+++ examples/Maestro/eg004CancelWorkflow.sh
@@ -7,9 +7,9 @@ if [[ $SHELL != *"bash"* ]]; then
 fi
 
 # Check that there is a workflow
-workflow_id=$(cat config/WORKFLOW_ID)
-if [ -z "$workflow_id" ]; then
-    echo "Please run example 1 to create and trigger a worklow before running this example."
+WORKFLOW_ID=$(cat config/WORKFLOW_ID)
+if [ -z "$WORKFLOW_ID" ]; then
+    echo "Please run example 1 to create and trigger a workflow before running this example."
     exit 0
 fi
 
@@ -27,9 +27,9 @@ ACCESS_TOKEN=$(cat config/ds_access_token.txt)
 
 # Set up variables for full code example
 # Note: Substitute these values with your own
-ACCOUNT_ID=$(cat config/API_ACCOUNT_ID)
+API_ACCOUNT_ID=$(cat config/API_ACCOUNT_ID)
 
-base_path="https://api-d.docusign.com/v1"
+BASE_PATH="https://api-d.docusign.com/v1"
 
 # Construct your API headers
 #ds-snippet-start:Maestro4Step2
@@ -38,16 +38,19 @@ declare -a Headers=('--header' "Authorization: Bearer ${ACCESS_TOKEN}" \
     '--header' "Content-Type: application/json")
 #ds-snippet-end:Maestro4Step2
 echo ""
-echo "Attempting to cancel the Workflow instance..."
+echo "Attempting to cancel the workflow instance..."
 echo ""
 
 #ds-snippet-start:Maestro4Step3
+#apx-snippet-start:cancelWorkflowInstance
 response=$(mktemp /tmp/response-wftmp.XXXXXX)
 Status=$(
-    curl -w '%{http_code}' --request POST "${base_path}/accounts/${ACCOUNT_ID}/workflows/${workflow_id}/instances/${WORKFLOW_INSTANCE_ID}/actions/cancel" \
+    curl -w '%{http_code}' --request POST "${BASE_PATH}/accounts/${API_ACCOUNT_ID}/workflows/${WORKFLOW_ID}/instances/${WORKFLOW_INSTANCE_ID}/actions/cancel" \
     "${Headers[@]}" \
     --output ${response}
 )
+#apx-snippet-end:cancelWorkflowInstance
+
 # If the status code returned is greater than 201 (OK / Accepted), display an error message with the API response.
 if [[ "$Status" -gt "201" ]]; then
     echo ""
